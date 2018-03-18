6a551d6cf2effe51efe6ab5c7544b24d3162673e
C-Laborde <cecilialaborde@gmail.com>
2018-03-18 20:32:58 +0100

    Basic text scrapping

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+# Tutorial in https://www.dataquest.io/blog/web-scraping-tutorial-python/
 
+
+import requests
+from bs4 import BeautifulSoup
+
+
+page = requests.get("http://dataquestio.github.io/web-scraping-pages/simple.html")
+
+if page.status_code == 200:
+    print("Page status code: %s Download correct" % page.status_code)
+else:
+    print("Page status code: %s Error downloading" % page.status_code)
+
+# print(page.content)
+
+soup = BeautifulSoup(page.conteng, 'html.parser')
+
+# print(soup.prettify())
+
+print('Elements types:')
+print([type(item) for item in list(soup.childre)])
+
+html = list(soup.children)[2]
+body = list(html.children)[3]
+p = list(body.children)[1]
+
+print(p.get_text())
