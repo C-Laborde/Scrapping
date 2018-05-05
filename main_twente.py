e10472a337348c65c345b5ff5dc9f78a1cd91712
C-Laborde <cecilialaborde@gmail.com>
2018-05-05 20:32:31 +0200

    dryscraper for js

--- main_twente.py
+++ main_twente.py
@@ -1,30 +1,25 @@
-# tutorial on https://realpython.com/python-web-scraping-practical-introduction/
+# tutorial on
+# https://realpython.com/python-web-scraping-practical-introduction/
 
 from bs4 import BeautifulSoup
-
 from aux_func.simple_get import simple_get
-
+import dryscrape
 
 page_address = "https://webapps.utwente.nl/reservationscalendar/"
-page_content = simple_get(page_address)
+
+session = dryscrape.Session()
+session.visit(page_address)
+response = session.body()
+soup = BeautifulSoup(response, 'html.parser')
+spans = soup.find('span')
+print(spans)
 
 
-# Possible parsers
-# soup = BeautifulSoup(page.content, 'lxml')
-# soup = BeautifulSoup(page.content, 'lxml-xml')
-page_content = BeautifulSoup(page.content, 'html.parser')
-# soup = BeautifulSoup(page.content, 'html5lib')
+"""raw_page_content = simple_get(page_address)
 
-# print(soup.prettify())
+parser = 'html.parser'
+parser = 'lxml'
+html_content = BeautifulSoup(raw_page_content, parser)
 
-# k_header = soup.find_all('div')
-# subclass = soup.find_all(class_="k-state-default k-nav-current")
-# print(soup.find_all('div', class_="k-reset k-scheduler-navigation"))
-# print(soup.find_all('li', class_="k-state-default k-nav-current"))
-# print(soup.find_all('body div'))
-print(page_content.find(class_="k-lg-date-format"))
-
-# print(soup.find_all('li')[0].get_text())
-# print(len(k_header))
-# print(k_header)
-# print(subclass)
+divs = html_content.find_all('span')
+print(divs)"""
